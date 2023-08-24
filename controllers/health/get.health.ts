e1425bb69a008fcb0e7394312f9396cbac61623c
Bernard Arhia <bernardarhia@gmail.com>
2023-08-24 23:44:26 +0000

    Update get.health.ts

--- controllers/health/get.health.ts
+++ controllers/health/get.health.ts
@@ -11,7 +11,7 @@ const data: IData = {
 };
 async function postHandler(req: Request, res: Response) {
   try {
-    return res.send({ success: true, message: "Health check modified, Welcome" });
+    return res.send({ success: true, message: "Health check modified, Welcome 1" });
   } catch (error: any) {
     res.status(500).send({ error: error.message });
   }
