65de726a416c29ea52cd94317305eb8829d2cddb
bernardarhia <barhia@infonaligy.com>
2023-09-23 04:09:38 +0000

    minor change

--- core/webhook.ts
+++ core/webhook.ts
@@ -40,15 +40,18 @@ export class PaystackWebhookHandler {
     );
 
     const user = await userService.findOne({ email: data.customer?.email });
+    const wallet = await walletService.findOne({_id: data.metadata?.walletId});
+
+    let walletNumbers = wallet?.type  === "credit card" ? wallet.cardDetails.number : wallet?.mobileMoneyDetails.network;
     await emailSender.walletTopup({
       email: data.customer?.email,
       amount: roundNumber(data.amount as number),
       recipientName: user?.firstName as string,
       transactionDate: new Date(),
       transactionStatus: data.status as string,
-      paymentMethod: data.channel?.split("_").join(" ").toUpperCase() as string,
-      paymentNumber: `${data.authorization?.bin}${data.authorization?.last4}`,
-      transactionId: data.reference as string
+      paymentMethod: determinePaymentMethod(data.authorization),
+      paymentNumber: maskString(walletNumbers as string),
+      transactionId: data.reference as string,
     });
   }
   private async handleChargeFailure(data: any) {
@@ -56,5 +59,31 @@ export class PaystackWebhookHandler {
     // send email
   }
 }
-
+function determinePaymentMethod(data?: Record<string, any>): string {
+  if (!data || !Object.keys(data).length) return "";
+  const channelArray = data.channel.split("_");
+  return channelArray.includes("mobile")
+    ? "Mobile Money"
+    : channelArray.includes("card")
+    ? `${data.channel}${data.card_type}`
+    : "Unknown";
+}
+function maskString(inputString: string) {
+  if(!inputString) return "";
+  // Check if the inputString has a length of 10
+  if (inputString.length === 10) {
+    return (
+      inputString.substring(0, 3) +
+      inputString.substring(4, 9).replace(/./g, "*") +
+      inputString.substring(8)
+    );
+  } else if (inputString.length > 10) {
+    return (
+      inputString.substring(0, 4) +
+      inputString.substring(4, inputString.length - 3).replace(/./g, "*") +
+      inputString.substring(inputString.length - 4)
+    );
+  }
+  return inputString;
+}
 export const paystackWebhook = new PaystackWebhookHandler();
